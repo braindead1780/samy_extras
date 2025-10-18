33de74796281bfe84b3072134e691a52852faba2
SamyHub <70079085+iitzSamy@users.noreply.github.com>
2025-10-19 00:21:34 +0200

    Update fxmanifest.lua to include main.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,15 @@
 fx_version 'cerulean'
 game 'gta5'
+
 description 'Samy_Extras'
 version '1.0.0'
 
 lua54 'yes'
 
-shared_scripts { '@ox_lib/init.lua' }
+shared_scripts {
+    '@ox_lib/init.lua',
+}
 
-client_scripts { '*.lua' }
+client_scripts {
+    'main.lua',
+}
